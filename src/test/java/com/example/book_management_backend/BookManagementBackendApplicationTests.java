df65b78bb530fcac71ce7d60ba65c3b6634d8a57
Abigail1207 <tladmswjd113@naver.com>
2025-11-03 13:32:28 +0000

    first commit

---
+++ src/test/java/com/example/book_management_backend/BookManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
